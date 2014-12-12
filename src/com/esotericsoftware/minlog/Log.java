a4cfce15f756a02b7a647d2b0610085f6585cd5a
NathanSweet <nathan.sweet@gmail.com>
2014-12-12 08:26:01 +0100

    Javadoc formatting

--- src/com/esotericsoftware/minlog/Log.java
+++ src/com/esotericsoftware/minlog/Log.java
@@ -5,10 +5,8 @@ import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.util.Date;
 
-/**
- * A low overhead, lightweight logging system.
- * @author Nathan Sweet <misc@n4te.com>
- */
+/** A low overhead, lightweight logging system.
+ * @author Nathan Sweet <misc@n4te.com> */
 public class Log {
 	/** No logging at all. */
 	static public final int LEVEL_NONE = 6;
@@ -23,10 +21,8 @@ public class Log {
 	/** Trace messages. A lot of information is logged, so this level is usually only needed when debugging a problem. */
 	static public final int LEVEL_TRACE = 1;
 
-	/**
-	 * The level of messages that will be logged. Compiling this and the booleans below as "final" will cause the compiler to
-	 * remove all "if (Log.info) ..." type statements below the set level.
-	 */
+	/** The level of messages that will be logged. Compiling this and the booleans below as "final" will cause the compiler to
+	 * remove all "if (Log.info) ..." type statements below the set level. */
 	static private int level = LEVEL_INFO;
 
 	/** True when the ERROR level will be logged. */
@@ -40,9 +36,7 @@ public class Log {
 	/** True when the TRACE level will be logged. */
 	static public boolean TRACE = level <= LEVEL_TRACE;
 
-	/**
-	 * Sets the level to log. If a version of this class is being used that has a final log level, this has no affect.
-	 */
+	/** Sets the level to log. If a version of this class is being used that has a final log level, this has no affect. */
 	static public void set (int level) {
 		// Comment out method contents when compiling fixed level JARs.
 		Log.level = level;
@@ -77,9 +71,7 @@ public class Log {
 		set(LEVEL_TRACE);
 	}
 
-	/**
-	 * Sets the logger that will write the log messages.
-	 */
+	/** Sets the logger that will write the log messages. */
 	static public void setLogger (Logger logger) {
 		Log.logger = logger;
 	}
@@ -169,10 +161,8 @@ public class Log {
 	private Log () {
 	}
 
-	/**
-	 * Performs the actual logging. Default implementation logs to System.out. Extended and use {@link Log#logger} set to handle
-	 * logging differently.
-	 */
+	/** Performs the actual logging. Default implementation logs to System.out. Extended and use {@link Log#logger} set to handle
+	 * logging differently. */
 	static public class Logger {
 		private long firstLogTime = new Date().getTime();
 
@@ -224,9 +214,7 @@ public class Log {
 			print(builder.toString());
 		}
 
-		/**
-		 * Prints the message to System.out. Called by the default implementation of {@link #log(int, String, String, Throwable)}.
-		 */
+		/** Prints the message to System.out. Called by the default implementation of {@link #log(int, String, String, Throwable)}. */
 		protected void print (String message) {
 			System.out.println(message);
 		}
