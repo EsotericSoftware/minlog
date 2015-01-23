3e50b7296d3fdabb535bee321e2c2a3a813cf20e
Martin Grotzke <martin.grotzke@googlemail.com>
2015-01-23 07:17:10 +0100

    Sets artifactId to minlog-none, disables logging.

--- src/com/esotericsoftware/minlog/Log.java
+++ src/com/esotericsoftware/minlog/Log.java
@@ -27,30 +27,32 @@ public class Log {
 	 * The level of messages that will be logged. Compiling this and the booleans below as "final" will cause the compiler to
 	 * remove all "if (Log.info) ..." type statements below the set level.
 	 */
-	static private int level = LEVEL_INFO;
+	static private final int level = LEVEL_NONE;
 
 	/** True when the ERROR level will be logged. */
-	static public boolean ERROR = level <= LEVEL_ERROR;
+	static public final boolean ERROR = level <= LEVEL_ERROR;
 	/** True when the WARN level will be logged. */
-	static public boolean WARN = level <= LEVEL_WARN;
+	static public final boolean WARN = level <= LEVEL_WARN;
 	/** True when the INFO level will be logged. */
-	static public boolean INFO = level <= LEVEL_INFO;
+	static public final boolean INFO = level <= LEVEL_INFO;
 	/** True when the DEBUG level will be logged. */
-	static public boolean DEBUG = level <= LEVEL_DEBUG;
+	static public final boolean DEBUG = level <= LEVEL_DEBUG;
 	/** True when the TRACE level will be logged. */
-	static public boolean TRACE = level <= LEVEL_TRACE;
+	static public final boolean TRACE = level <= LEVEL_TRACE;
 
 	/**
 	 * Sets the level to log. If a version of this class is being used that has a final log level, this has no affect.
 	 */
 	static public void set (int level) {
 		// Comment out method contents when compiling fixed level JARs.
+		/*
 		Log.level = level;
 		ERROR = level <= LEVEL_ERROR;
 		WARN = level <= LEVEL_WARN;
 		INFO = level <= LEVEL_INFO;
 		DEBUG = level <= LEVEL_DEBUG;
 		TRACE = level <= LEVEL_TRACE;
+		*/
 	}
 
 	static public void NONE () {
